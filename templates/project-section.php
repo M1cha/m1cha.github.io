d653c74958e1cab72a8a645e75888d4bdf27366b
M1cha <sigmaepsilon92@gmail.com>
2015-07-08 15:30:15 +0200

    templates: project-section: add section anchors

--- templates/project-section.php
+++ templates/project-section.php
@@ -1,4 +1,5 @@
 <div class="android-more-section">
+    <a name="<?=makeurlname($title)?>"></a>
     <div class="android-section-title mdl-typography--display-1-color-contrast"><?=$this->e($title)?></div>
     <div class="android-card-container mdl-grid">
         <?=$cards?>
