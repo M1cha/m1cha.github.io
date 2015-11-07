a56ae6e51cc008a9dcde208121a06570ab049cc7
M1cha <sigmaepsilon92@gmail.com>
2015-11-07 05:43:06 +0100

    templates: contact: remove unused 'email.js'

--- templates/contact.php
+++ templates/contact.php
@@ -1,7 +1,5 @@
 <?php $this->layout('layout_article', ["themecolor"=>"#F57921", "title"=>"Contact"]) ?>
 
-<script src="//www.bronze-age.com/email.js"></script>
-
 <a href="https://plus.google.com/+MichaelZimmermann/posts">Google+</a><br>
 <a href="https://github.com/M1cha">Github</a><br>
 <?=protectedMail("sigmaepsilon92@gmail.com")?>
