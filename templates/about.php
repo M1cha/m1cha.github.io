f15eb03e9bda094e2b8f0829623056c72523a678
M1cha <sigmaepsilon92@gmail.com>
2015-11-08 12:35:59 +0100

    add back title to the pages 'about' and 'contact'

--- templates/about.php
+++ templates/about.php
@@ -1,3 +1,4 @@
 <?php $this->layout('layout_article', ["themecolor"=>"#F57921", "title"=>"About"]) ?>
+<h3>About</h3>
 
 <p>I'm a software engineer with a passion for open source.</p>
--- templates/contact.php
+++ templates/contact.php
@@ -1,4 +1,5 @@
 <?php $this->layout('layout_article', ["themecolor"=>"#F57921", "title"=>"Contact"]) ?>
+<h3>Contact</h3>
 
 <a href="https://plus.google.com/+MichaelZimmermann/posts">Google+</a><br>
 <a href="https://github.com/M1cha">Github</a><br>
